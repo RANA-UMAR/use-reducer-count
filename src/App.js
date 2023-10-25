344ee1517afec6360126455c646548e742254381
Rana Umar <ranaumar1126@gmail.com>
2023-10-26 01:48:57 +0500

    use reduce count

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
-import React, { useState } from "react";
-import data from "./data";
+import React,{useReducer} from 'react'
 
+const initialState = 0;
+const reducer = (state,action)=>{
+  if(action.type==="increment"){
+    return state+1;
+  }
+  if(action.type==="decrement"){
+    return state-1;
+  }
+  return state;
+
+}
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [paragrapgh, setParagrapgh] = useState([]);
+   
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let amount = parseInt(count)
-    console.log(typeof count);
-    if(amount<=0){
-      amount = 1;
-    }
-    if(amount>9){
-      amount = 9;
-    }
-    setParagrapgh(data.slice(0,amount));
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="text-center text-2xl font-semibold my-5">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="amount">Paragrapgh</label>
-        <input
-          type="number"
-          name="amount"
-          id="amount"
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
-          className="text-center mx-5 w-24 bg-slate-100 rounded-md"
-        />
-        <button
-          type="submit"
-          className="bg-green-500 text-white p-2 rounded-md"
-        >
-          Generate
-        </button>
-      </form>
-      <div>
-        {paragrapgh.map((item, index) => {
-          return <p className="font-normal" key={index}>{item}</p>;
-        })}
+    <div className='text-center flex justify-center items-center flex-col my-20'>
+    <p className='font-semibold mb-3'>BY USING USE REDUCER HOOK</p>
+      <p className='text-3xl font-bold '>{state}</p>
+      <div className='space-x-5 my-6'>
+        <button className='p-3 bg-sky-600 text-white' onClick={()=>dispatch({type:"increment"})}>INCREMENT</button>
+        <button className='p-3 bg-sky-600 text-white' onClick={()=>dispatch({type:"decrement"})}>DECREMENT</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
